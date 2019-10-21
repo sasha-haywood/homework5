0207fa02441b1b58d1ec72aa052d5e8dee505c09
Sasha Haywood <haywood@iu.edu>
2019-10-21 12:30:24 -0400

    corrected typos in llr

--- llr_functions.R
+++ llr_functions.R
@@ -1,10 +1,10 @@
 llr = function(x, y, z, omega){
-  fits - sapply(z, compute_f_hat, x, y, omega)
+  fits = sapply(z, compute_f_hat, x, y, omega)
   return(fits)
 }
 compute_f_hat = function(z, x, y, omega) {
   Wz = make_weight_matrix(z, x, omega)
-  X - make_predictor_matrix(x)
+  X = make_predictor_matrix(x)
   f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
   return(f_hat)
 }
