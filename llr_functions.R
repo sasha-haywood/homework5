526b20f2f13b845a3a2fae5f04662abb70427c20
Sasha Haywood <haywood@iu.edu>
2019-10-21 11:48:15 -0400

    function and test written

---
+++ llr_functions.R
@@ -0,0 +1,27 @@
+llr = function(x, y, z, omega){
+  fits - sapply(z, compute_f_hat, x, y, omega)
+  return(fits)
+}
+compute_f_hat = function(z, x, y, omega) {
+  Wz = make_weight_matrix(z, x, omega)
+  X - make_predictor_matrix(x)
+  f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  return(f_hat)
+}
+make_weight_matrix = function(z, x, omega) {
+  r = abs(x - z) / omega
+  w = sapply(r, W)
+  Wz = diag(w)
+  return(Wz)
+}
+W = function(r) {
+  if (abs(r) < 1) {
+    return((1 - abs(r) ** 3) ** 3)
+  } else{
+    return(0)
+  }
+}
+make_predictor_matrix = function(x) {
+  n = length(x)
+  return(cbind(rep(1, n), x))
+}
---
+++ test_llr.R
@@ -0,0 +1,35 @@
+library(testthat)
+import::here(compute_f_hat, llr, make_predictor_matrix, make_weight_matrix, W,             
+             .from = 'llr_functions.R')
+context("Check local linear regression function")
+n = 15
+x = rnorm(n)
+y = rnorm(x + rnorm(n))
+z = seq(-1, 1, length.out = 100)
+omega = 1
+# --- tests --- #
+test_that("llr output has correct length", {  
+  expect_equal(length(llr(x, y, z, omega = 1)), length(z))
+  })
+test_that("make_weight_matrix works on simple cases", {  
+  # make an example weight matrix  
+  Wz = make_weight_matrix(z[1], x, omega)  
+  # check that the output is a diagonal matrix,   
+  expect_true(all(c(Wz[upper.tri(Wz)], Wz[lower.tri(Wz)]) == 0))  
+  # that all the elements are nonnegative,   
+  expect_true(all(Wz >= 0))  
+  # that the weights are correct in simple cases   
+  # where you know what the output should be  
+  r = abs(x - z[1]) / omega  
+  expect_equal(diag(Wz), sapply(r, W))
+  })
+test_that("make_predictor_matrix works on simple cases", {  
+  # make an example predictor matrix  
+  X = make_predictor_matrix(x)  
+  # write tests to check that the dimensions are correct,   
+  expect_equal(dim(X), c(n, 2))  
+  # the first column is all 1's,   
+  expect_equal(X[, 1], rep(1, n))  
+  # second column is x  
+  expect_equal(X[, 2], x)
+  })
