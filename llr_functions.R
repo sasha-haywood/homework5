b79cb934164b1432d596c82b802ab3a7e870b54f
Sasha Haywood <haywood@iu.edu>
2019-10-24 14:52:07 -0400

    change llr to be faster - test 1

--- llr_functions.R
+++ llr_functions.R
@@ -3,9 +3,10 @@ llr = function(x, y, z, omega){
   return(fits)
 }
 compute_f_hat = function(z, x, y, omega) {
-  Wz = make_weight_matrix(z, x, omega)
+  Wz = diag(make_weight_matrix(z, x, omega))
   X = make_predictor_matrix(x)
-  f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  f_hat = c(1, z) %*% solve(t(X) %*% apply(X, 2, function(Xcol){Wz*Xcol})) %*% t(X) %*% 
+    apply(as.matrix(y),2, function(Xcol){Wz*Xcol})
   return(f_hat)
 }
 make_weight_matrix = function(z, x, omega) {
